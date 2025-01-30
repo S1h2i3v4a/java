e4c277d9f9be25c78c433dc3713744d10fdf45d7
S1h2i3v4a <shivamkeshari990@gmail.com>
2025-01-30 21:47:46 +0530

    list2

---
+++ src/Main2.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main2 {
+    public static void main(String[] args) {
+        List strlist = new ArrayList();
+        strlist.add("Shivam");
+        strlist.add("Keshari");
+        strlist.add(210);
+        Main.display(strlist);
+    }
+}
